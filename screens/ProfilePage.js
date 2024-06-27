350318aa5c824fea5f6ce6fd8842ced76ef0acca
Adam Hocini <adam.hocini.ah@icloud.com>
2024-06-27 09:33:13 +0200

    contentContainerProfilPage

--- screens/ProfilePage.js
+++ screens/ProfilePage.js
@@ -4,32 +4,17 @@ import * as SecureStore from "expo-secure-store";
 import { useNavigation } from "@react-navigation/native";
 import { useUser } from "../context/Context"; // Import du hook useUser
 import { SafeAreaView } from 'react-native-safe-area-context';
-import EditProfilePage from "./EditProfilePage";
-import SettingsPage from "./SettingsPage";
 import MainMenu2 from "../components/MainMenu2";
 
-
 const ProfilePage = () => {
-	const { user } = useUser(); // Utilisation du hook useUser pour accéder aux données de l'utilisateur
-	const navigation = useNavigation();
+    const { user } = useUser(); // Utilisation du hook useUser pour accéder aux données de l'utilisateur
+    const navigation = useNavigation();
 
-	const handleLogout = async () => {
-		await SecureStore.deleteItemAsync("accessToken");
-		await SecureStore.deleteItemAsync("refreshToken");
-		await SecureStore.deleteItemAsync("userData"); // Supprimer les données utilisateur de SecureStore lors de la déconnexion
-		navigation.navigate("WelcomePage");
-	};
-	
-	if (!user) {
-		return (
-			<View style={styles.container}>
-				<Text style={styles.description}>Chargement...</Text>
-			</View>
-		);
-	}
-
-	const handleBackPress = () => {
-        navigation.navigate("HomePage");
+    const handleLogout = async () => {
+        await SecureStore.deleteItemAsync("accessToken");
+        await SecureStore.deleteItemAsync("refreshToken");
+        await SecureStore.deleteItemAsync("userData"); // Supprimer les données utilisateur de SecureStore lors de la déconnexion
+        navigation.navigate("WelcomePage");
     };
 
     if (!user) {
@@ -39,190 +24,147 @@ const ProfilePage = () => {
             </View>
         );
     }
-	
-       
-    
 
-	return (
-		<SafeAreaView style={styles.safeArea}>
-			<ScrollView contentContainerStyle={styles.container}>
-			<SafeAreaView style={styles.safeArea}>
+    const handleBackPress = () => {
+        navigation.navigate("HomePage");
+    };
+
+    return (
+        <SafeAreaView style={styles.safeArea}>
             <MainMenu2 />
-            <ScrollView contentContainerStyle={styles.container}>
-                {/* Reste du contenu de la page */}
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                <View style={styles.profileContainer}>
+                    <Image source={require('../assets/photodeprofil/photodeprofil1.png')} style={styles.profileImage} />
+                    <Text style={styles.profileName}>Madison Smith</Text>
+                    <Text style={styles.profileEmail}>{user.email}</Text>
+                    <Text style={styles.profileBirthday}>Birthday: April 1st</Text>
+                </View>
+                <View style={styles.statsContainer}>
+                    <View style={styles.statBox}>
+                        <Text style={styles.statValue}>75 Kg</Text>
+                        <Text style={styles.statLabel}>Weight</Text>
+                    </View>
+                    <View style={styles.statBox}>
+                        <Text style={styles.statValue}>28</Text>
+                        <Text style={styles.statLabel}>Years Old</Text>
+                    </View>
+                    <View style={styles.statBox}>
+                        <Text style={styles.statValue}>1.65 CM</Text>
+                        <Text style={styles.statLabel}>Height</Text>
+                    </View>
+                </View>
+                <View style={styles.menuContainer}>
+                    <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("EditProfilePage")}>
+                        <View style={styles.menuItemIcon}>
+                            <Image source={require('../assets/icon/decision-making_16330913.png')} style={styles.icon} />
+                        </View>
+                        <Text style={styles.menuItemText}>Edit Profile</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("EditProfilePage")}>
+                        <View style={styles.menuItemIcon}>
+                            <Image source={require('../assets/icon/favorite_222298.png')} style={styles.icon} />
+                        </View>
+                        <Text style={styles.menuItemText}>Favorite</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("EditProfilePage")}>
+                        <View style={styles.menuItemIcon}>
+                            <Image source={require('../assets/icon/shield_109123.png')} style={styles.icon} />
+                        </View>
+                        <Text style={styles.menuItemText}>Privacy Policy</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("SettingsPage")}>
+                        <View style={styles.menuItemIcon}>
+                            <Image source={require('../assets/icon/setting_4255672.png')} style={styles.icon} />
+                        </View>
+                        <Text style={styles.menuItemText}>Settings</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("EditProfilePage")}>
+                        <View style={styles.menuItemIcon}>
+                            <Image source={require('../assets/icon/help_2081580.png')} style={styles.icon} />
+                        </View>
+                        <Text style={styles.menuItemText}>Help</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.menuItem} onPress={handleLogout}>
+                        <View style={styles.menuItemIcon}>
+                            <Image source={require('../assets/icon/icon.logout.png')} style={styles.icon} />
+                        </View>
+                        <Text style={styles.menuItemText}>Logout</Text>
+                    </TouchableOpacity>
+                </View>
             </ScrollView>
         </SafeAreaView>
-
-				<View style={styles.header}>
-					<TouchableOpacity onPress={handleBackPress} style={styles.backButton}>
-                        <Image source={require('../assets/icon/home_222306.png')} style={styles.backIcon} />
-                    </TouchableOpacity>
-                    <Text style={styles.headerText}>My Profile</Text>				
-				</View>
-				<View style={styles.profileContainer}>
-					<Image source={require('../assets/photodeprofil/photodeprofil1.png')} style={styles.profileImage} />
-					<Text style={styles.profileName}>Madison Smith</Text>
-					<Text style={styles.profileEmail}>{user.email}</Text>
-					<Text style={styles.profileBirthday}>Birthday: April 1st</Text>
-				</View>
-				<View style={styles.statsContainer}>
-					<View style={styles.statBox}>
-						<Text style={styles.statValue}>75 Kg</Text>
-						<Text style={styles.statLabel}>Weight</Text>
-					</View>
-					<View style={styles.statBox}>
-						<Text style={styles.statValue}>28</Text>
-						<Text style={styles.statLabel}>Years Old</Text>
-					</View>
-					<View style={styles.statBox}>
-						<Text style={styles.statValue}>1.65 CM</Text>
-						<Text style={styles.statLabel}>Height</Text>
-					</View>
-				</View>
-				<View style={styles.menuContainer}>
-					{/* {menuItems.map((item, index) => (
-						<TouchableOpacity key={index} style={styles.menuItem} onPress={navigation.navigate(link)}>
-							<View style={styles.menuItemIcon}>
-								<Image source={item.icon} style={styles.icon} />
-							</View>
-							<Text style={styles.menuItemText}>{item.label}</Text>
-						</TouchableOpacity>
-					))} */}
-					<TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("EditProfilePage")}>
-						<View style={styles.menuItemIcon}>
-							<Image source={require('../assets/icon/decision-making_16330913.png')} style={styles.icon} />
-						</View>
-						<Text style={styles.menuItemText}>Edite Profile</Text>
-					</TouchableOpacity>
-					
-					<TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("EditProfilePage")}>
-						<View style={styles.menuItemIcon}>
-							<Image source={require('../assets/icon/favorite_222298.png')} style={styles.icon} />
-						</View>
-						<Text style={styles.menuItemText}>Favorite</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("EditProfilePage")}>
-						<View style={styles.menuItemIcon}>
-							<Image source={require('../assets/icon/shield_109123.png')} style={styles.icon} />
-						</View>
-						<Text style={styles.menuItemText}>Privacy Policy</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("SettingsPage")}>
-						<View style={styles.menuItemIcon}>
-							<Image source={require('../assets/icon/setting_4255672.png')} style={styles.icon} />
-						</View>
-						<Text style={styles.menuItemText}>Settings</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate("EditProfilePage")}>
-						<View style={styles.menuItemIcon}>
-							<Image source={require('../assets/icon/help_2081580.png')} style={styles.icon} />
-						</View> 
-						<Text style={styles.menuItemText}>Help</Text>
-					</TouchableOpacity>
-
-					<TouchableOpacity style={styles.menuItem} onPress={handleLogout}>
-						<View style={styles.menuItemIcon}>
-							<Image source={require('/Users/adamhocini/Documents/Isitech/B3/Gym/assets/icon/icon.logout.png')} style={styles.icon} />
-						</View>
-						<Text style={styles.menuItemText}>Logout</Text>
-					</TouchableOpacity>
-				</View>
-			</ScrollView>
-		</SafeAreaView >
-	);
+    );
 };
 
-
 const styles = StyleSheet.create({
-	safeArea: {
+    safeArea: {
         flex: 1,
-        backgroundColor: '#004080',
-		paddingVertical: -40,
+        backgroundColor: '#f0f0f0',
     },
-	container: {
-		flexGrow: 1,
-		backgroundColor: '#FFF9F2',
-	},
-	header: {
-		backgroundColor: '#004080',
-		paddingVertical: 30,
-		alignItems: 'flex-start',
-		paddingHorizontal: 30,
-		flexDirection: 'row',
-
-	},
-	backButton: {
-        marginRight: 10,
+    scrollContainer: {
+        flexGrow: 1,
+        paddingVertical: 20,
     },
-	backIcon: {
+    profileContainer: {
+        backgroundColor: '#f0f0f0',
+        alignItems: 'center',
+    },
+    profileImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+    },
+    profileName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginVertical: 5,
+    },
+    profileEmail: {
+        color: 'gray',
+    },
+    profileBirthday: {
+        color: 'gray',
+    },
+    statsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: '#0a539d',
+        paddingVertical: 10,
+        borderRadius: 20,
+        marginVertical: 15,
+        marginHorizontal: 15,
+    },
+    statBox: {
+        alignItems: 'center',
+    },
+    statValue: {
+        color: '#FFF9F2',
+        fontWeight: 'bold',
+    },
+    statLabel: {
+        color: '#FFF9F2',
+    },
+    menuContainer: {
+        backgroundColor: '#f0f0f0',
+        paddingHorizontal: 20,
+    },
+    menuItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: '#e0e0e0',
+    },
+    menuItemIcon: {
+        marginRight: 20,
+    },
+    menuItemText: {
+        fontSize: 16,
+    },
+    icon: {
         width: 24,
         height: 24,
     },
-	headerText: {
-		color: '#FFF9F2',
-		fontSize: 20,
-	},
-	profileContainer: {
-		backgroundColor: '#FFF9F2',
-		alignItems: 'center',
-		marginVertical: 20,
-	},
-	profileImage: {
-		width: 100,
-		height: 100,
-		borderRadius: 500,
-	},
-	profileName: {
-		fontSize: 20,
-		fontWeight: 'bold',
-		marginVertical: 5,
-	},
-	profileEmail: {
-		color: 'gray',
-	},
-	profileBirthday: {
-		color: 'gray',
-	},
-	statsContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-around',
-		backgroundColor: '#0a539d',
-		paddingVertical: 10,
-		borderRadius: 20,
-		marginVertical: 15,
-		marginHorizontal: 15,
-	},
-	statBox: {
-		alignItems: 'center',
-	},
-	statValue: {
-		color: '#FFF9F2',
-		fontWeight: 'bold',
-	},
-	statLabel: {
-		color: '#FFF9F2',
-	},
-	menuContainer: {
-		backgroundColor: '#FFF9F2',
-		paddingHorizontal: 20,
-	},
-	menuItem: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		paddingVertical: 15,
-		borderBottomWidth: 1,
-		borderBottomColor: '#f0f0f0',
-	},
-	menuItemIcon: {
-		marginRight: 20,
-	},
-	menuItemText: {
-		fontSize: 16,
-	},
-	icon: {
-		width: 24,
-		height: 24,
-	},
 });
 
 export default ProfilePage;
